4dd2cf3c9a2b41538958492c9a84ce20f87324a1
marechal229-cmyk <marechal229@gmail.com>
2026-08-23 22:31:16 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Plume Libre</h1>
+      <p>Bienvenue sur Plume Libre.</p>
+      <p>La plateforme de publication et de lecture pour auteurs indépendants.</p>
+    </main>
+  );
+}
